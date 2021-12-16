e94dc4eb223ce694037dd8fe1e1c92544af04b1e
DAKorotkovMIEM <dakorotkov_1@edu.hse.ru>
2021-12-16 18:34:18 +0300

    restored questions

--- Questions.py
+++ Questions.py
@@ -1,4 +1,5 @@
 import sqlite3
+from basic_functions import send_message
 conn = sqlite3.connect('users.db')
 cur = conn.cursor()
 
@@ -6,3 +7,5 @@ cur = conn.cursor()
 def ask_questions(user_id, msg):
         cur.execute("""INSERT INTO questions(id, question)
                                  VALUES(?, ?);""", (user_id, msg))
+        send_message(184299452, f'От: {user_id}. Вопрос: {msg}')
+        send_message(user_id, 'Вопрос отправлен')
