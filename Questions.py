31146dadf5a14d2a30677ef69cbf102f377d4d84
DAKorotkovMIEM <dakorotkov_1@edu.hse.ru>
2021-12-16 19:27:16 +0300

    restored questions

--- Questions.py
+++ Questions.py
@@ -5,7 +5,8 @@ cur = conn.cursor()
 
 
 def ask_questions(user_id, msg):
-        cur.execute("""INSERT INTO questions(id, question)
-                                 VALUES(?, ?);""", (user_id, msg))
-        send_message(184299452, f'От: {user_id}. Вопрос: {msg}')
+        cur.execute("""INSERT INTO questions(id, question, answer)
+                                 VALUES(?, ?, ?);""", (user_id, msg, None))
+        name = cur.execute(f"SELECT fullname FROM user_role WHERE id={user_id}").fetchone()[0]
+        send_message(184299452, f'От: {name}. Вопрос: {msg}')
         send_message(user_id, 'Вопрос отправлен')
