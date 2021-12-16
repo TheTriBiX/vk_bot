77cb7ce559025919b1b6e9304b8e66273585a8c6
TheTriBiX <57715605+TheTriBiX@users.noreply.github.com>
2021-12-16 15:00:58 +0300

    added registration and basic_functions.py

--- main.py
+++ main.py
@@ -2,41 +2,27 @@ import vk_api
 import time
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from basic_functions import send_message
+from registration import registration
 import sqlite3
 
-with open('token.txt') as f:
-    API_TOKEN = str(f.readline())
-vk = vk_api.VkApi(token=API_TOKEN)
 conn = sqlite3.connect('users.db')
 cur = conn.cursor()
 first_time = True
 
-def send_message(user_id, message, keyboard=None):
-    post = {
-        'user_id': user_id,
-        'message': message,
-        'random_id': int(round(time.time() * 1000))
-    }
-    if keyboard:
-        post["keyboard"] = keyboard.get_keyboard()
-    vk.method('messages.send', post)
 
+with open('token.txt') as f:
+    API_TOKEN = str(f.readline())
+    vk = vk_api.VkApi(token=API_TOKEN)
 
 if __name__ == '__main__':
     print('starting...')
     for event in VkLongPoll(vk).listen():
-        if event.type == VkEventType.USER_ONLINE and first_time:
-            print('Пользователь', event.user_id, 'онлайн', event.platform)
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
             msg = event.text.lower()
             user_id = event.user_id
-
-            if msg == "начать":
-                send_message(user_id, 'Начинаем регистрацию!')
-                keyboard = VkKeyboard(one_time=True)
-                keyboard.add_button('Староста', VkKeyboardColor.PRIMARY)
-                keyboard.add_button('Ученик', VkKeyboardColor.NEGATIVE)
-                send_message(user_id, 'тыкни кнопку', keyboard)
+            if msg and not cur.execute(f"""SELECT id FROM user_role WHERE id={1}""").fetchone():
+                registration()
 
             if msg in ['староста', 'ученик']:
                 role = msg
