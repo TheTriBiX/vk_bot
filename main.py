e2b4ecdf828e24813e6df13a9c2fb9cb0f5b6187
DAKorotkovMIEM <dakorotkov_1@edu.hse.ru>
2021-12-16 19:34:05 +0300

    extra commit

--- main.py
+++ main.py
@@ -36,7 +36,7 @@ if __name__ == '__main__':
 
             if quest == 1:
                 ask_questions(user_id, msg)
-                send_message(184299452, "мы уведомили старосту, жди ответа")
+                send_message(user_id, "мы уведомили старосту, жди ответа")
                 quest = 0
 
             if msg == "начать":
