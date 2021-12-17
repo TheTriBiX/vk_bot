988bf96e0740126392e489ae00efdd113e3a51e5
DAKorotkovMIEM <dakorotkov_1@edu.hse.ru>
2021-12-17 14:36:46 +0300

    working questions function

--- Questions.py
+++ Questions.py
@@ -24,8 +24,9 @@ def checktell_answer():
 
 
 def answer_question(question, answer):
-    user_id = cur.execute(f"SELECT id FROM questions WHERE question={question}").fetchall()
+    print(question)
+    user_id = cur.execute(f"SELECT id FROM questions WHERE question='{question}'").fetchall()
     for user in user_id:
         send_message(user[0], f'Ты задавал вопрос: {question}. '
                               f'Дорогой(ая) и любимый(ая) староста ответил(а) на твой вопрос: {answer}')
-    cur.execute(f'DELETE FROM questions WHERE question={question}')
+    cur.execute(f"DELETE FROM questions WHERE question='{question}'")
