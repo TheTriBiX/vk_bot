3cc3a541458a86c40e2bf817f45ed55b60ae61f5
TheTriBiX <57715605+TheTriBiX@users.noreply.github.com>
2021-12-18 20:12:52 +0300

    timetable upgrade

--- timetable.py
+++ timetable.py
@@ -9,18 +9,14 @@ def create_timetable(name, today_date):
      name - ФИО ученика, чье расписание нужно вывести
      today_date - дата на которую нужно получить расписание
      """
-    table = ''
+    table = None
     id_request = requests.get(f'https://ruz.hse.ru/api/search?term={name}&type=student')
     user_id = id_request.json()[0]['id']
     r = requests.get(f'https://ruz.hse.ru/api/schedule/student/{user_id}?start={today_date}&finish={today_date}&lng=1')
     if r.json():
+        table = 'Расписание на сегодня:\n'
         for i in r.json():
             table += f'''{i['discipline']}({i['kindOfWork']}) 
             {i['beginLesson']} - {i['endLesson']}
             Ссылка на занятие - {i['url1']}\n'''
-        return 'Расписание на сегодня:\n' + table
-    else:
-        return None
-
-
-print(create_timetable('Семенищев Матвей Владимирович', '2021.12.18'))
+    return table
